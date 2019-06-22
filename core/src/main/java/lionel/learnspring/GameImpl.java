0857724f002a2e4ae0345e195924a04c1ecc0282
Lionel B <lionel.bersee@reece.com.au>
2019-06-22 15:58:23 +1000

    autowire `numberGenerator` property

--- core/src/main/java/lionel/learnspring/GameImpl.java
+++ core/src/main/java/lionel/learnspring/GameImpl.java
@@ -2,6 +2,7 @@ package lionel.learnspring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,6 +13,7 @@ public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -37,11 +39,6 @@ public class GameImpl implements Game {
     @PreDestroy
     public void preDestroy() {
         log.info("in Game preDestroy()");
-    }
-
-    // == public methods ==
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
     }
 
     @Override
