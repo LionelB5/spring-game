27e661e39fef319744699ff0c5661d5ab8ccc859
Lionel1232 <lionel1232@gmail.com>
2019-06-22 16:53:29 +1000

    remove `beans.xml` and use a java annotation configuration instead

---
+++ core/src/main/java/lionel/learnspring/AppConfig.java
@@ -0,0 +1,22 @@
+package lionel.learnspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lionel.learnspring")
+public class AppConfig {
+
+    // == bean methods ==
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
+
+}
